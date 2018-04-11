ce5f58990742e57e81bf975bdf23416933cbd51f
LalithaRajesh <36064287+LalithaRajesh@users.noreply.github.com>
2018-04-11 17:58:02 +0530

    Delete Traincascade.ps1

--- Traincascade.ps1
+++
@@ -1,4 +0,0 @@
-$opencv_traincascade = 'C:\Users\lalit\Miniconda3\Library\bin\opencv_traincascade.exe'
-
-& $opencv_traincascade -data cascade4 -vec Pos52.vec -bg bg.txt -numPos 100 -numNeg 1799 -numStages 10 -w 50 -h 50
-  
