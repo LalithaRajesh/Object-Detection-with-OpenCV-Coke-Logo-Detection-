0ff9ef0515ff3df206626bff8fe5cff13b81d497
LalithaRajesh <36064287+LalithaRajesh@users.noreply.github.com>
2018-04-11 17:57:16 +0530

    Delete CreateSamples.ps1

--- CreateSamples.ps1
+++
@@ -1,5 +0,0 @@
-$infofile = 'info.data'
-$opencv_createsamples = 'C:\Users\lalit\Miniconda3\Library\bin\opencv_createsamples.exe'
-$numpics = 1799
-& $opencv_createsamples -info $infofile -num $numpics -vec Pos52.vec -maxxangle 0.6 -maxyangle 0 -maxzangle 0.3 -maxidev 60 -bgcolor 0 -bgthresh 0 -w 50 -h 50 
-    
